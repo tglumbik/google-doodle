b5ecc21986655a3b132ffeab0717a1f8b920c1ac
tglumbik <60484108+tglumbik@users.noreply.github.com>
2021-02-19 09:16:32 -0800

    animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
 console.log('OK');
+
+gsap.to ('.bunnyCover', {
+    x: -250,
+    // delay: 2,
+    // duration: 2
+})
+
+gsap.to ('.ddgLogo', {
+    rotation: 360,
+    ease: "none",
+    duration: 2,
+    repeat: -1
+})
+
+gsap.to ('.recordGrooves', {
+    rotation: 360,
+    ease: "none",
+    duration: 2,
+    repeat: -1,
+    transformOrigin: "50% 50%"
+})
