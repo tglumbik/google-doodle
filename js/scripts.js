b8491d5db17905f430b8e6fec12b1195409ba6d5
tglumbik <60484108+tglumbik@users.noreply.github.com>
2021-02-19 10:16:59 -0800

    Rough

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,14 @@ console.log('OK');
 
 gsap.to ('.bunnyCover', {
     x: -250,
-    // delay: 2,
-    // duration: 2
+    delay: 2,
+    duration: 2
 })
 
 gsap.to ('.ddgLogo', {
     rotation: 360,
     ease: "none",
-    duration: 2,
+    duration: 2.5,
     repeat: -1
 })
 
@@ -19,4 +19,11 @@ gsap.to ('.recordGrooves', {
     duration: 2,
     repeat: -1,
     transformOrigin: "50% 50%"
+})
+
+gsap.from ('.recordText', {
+    opacity: 0,
+    ease: "power1.out",
+    duration: 2,
+    delay: 3.5
 })
