7e4e4b5ef4221238f528d68da06e5e8571c8ca38
tglumbik <60484108+tglumbik@users.noreply.github.com>
2021-03-15 09:49:11 -0700

    JS, Favicons, CSS

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,43 @@
 console.log('OK');
 
-gsap.to ('.bunnyCover', {
-    x: -250,
+// Random Fill
+
+const maxNumRand = 4;
+
+let numRand = Math.floor(Math.random()*maxNumRand);
+
+console.log(numRand);
+
+// Record Label
+
+const recordLabel = document.querySelector('#recordLabel');
+
+console.log(recordLabel);
+
+recordLabel.classList = 'recordLabel' + numRand;
+
+// Record Cover
+
+const recordCover = document.querySelector('#bunnyCover');
+
+console.log(recordCover);
+
+recordCover.classList = 'bunnyCover' + numRand;
+
+// Animation
+
+gsap.to ('#bunnyCover', {
+    x: -275,
     delay: 2,
     duration: 2
-})
+});
 
 gsap.to ('.ddgLogo', {
     rotation: 360,
     ease: "none",
     duration: 2.5,
     repeat: -1
-})
+});
 
 gsap.to ('.recordGrooves', {
     rotation: 360,
@@ -19,11 +45,11 @@ gsap.to ('.recordGrooves', {
     duration: 2,
     repeat: -1,
     transformOrigin: "50% 50%"
-})
+});
 
 gsap.from ('.recordText', {
     opacity: 0,
     ease: "power1.out",
     duration: 2,
     delay: 3.5
-})
+});
